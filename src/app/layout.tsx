8e8e4e756d3f43ee3b29929f31207f6f1e48b12e
niabnn <estefania.goez@udea.edu.co>
2025-04-21 03:22:38 -0500

    feat: add Sidebar component and update layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Nav from "@/components/layout/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import Providers from "@/providers/providers";
+import Sidebar from "@/components/layout/Sidebar";
 
 export const metadata: Metadata = {
   title: "SOPA",
@@ -15,12 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <Providers>
-      <html lang="en">
+      <html lang="es">
         <body className="min-h-screen bg-primary-200 text-primary-foreground">
-          <Nav />
-          <main className="min-h-[calc(100vh-4.25rem)] h-0 px-6 sm:px-12 md:px-24 lg:px-32 xl:px-56 py-6 overflow-auto">
-            {children}
-          </main>
+          <div className="flex h-screen overflow-hidden">
+            {/* Sidebar */}
+            <Sidebar />
+
+            {/*  principal content  */}
+            <div className="flex-1 overflow-auto">
+
+              {/* site content */}
+              <main className="px-6 sm:px-12 md:px-24 lg:px-32 xl:px-56 py-6">
+                {children}
+              </main>
+            </div>
+          </div>
         </body>
       </html>
     </Providers>
