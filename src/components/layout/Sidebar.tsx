6474ae95c55521f3d22ac0c12d7ff486d4159399
niabnn <estefania.goez@udea.edu.co>
2025-04-30 23:59:36 -0500

    refactor: update global styles and layout structure; enhance Sidebar with new links and improve Tailwind configuration

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -25,12 +25,23 @@ import {
   
           {/* Menu */}
           <nav className="space-y-2">
+
+            <a
+              href="#"
+              className="flex items-center justify-between px-3 py-2 rounded-md text-sky-900 hover:bg-gray-100"
+            >
+              <div className="flex items-center space-x-3">
+                <UsersIcon className="h-5 w-5" />
+                <span>Programación académica</span>
+              </div>
+            </a>
+
             <a
               href="#"
               className="flex items-center space-x-3 px-3 py-2 rounded-md text-sky-900 hover:bg-gray-100 font-medium"
             >
               <Squares2X2Icon className="h-5 w-5" />
-              <span>Pesum</span>
+              <span>Pensum</span>
             </a>
   
             <a
@@ -40,24 +51,17 @@ import {
               <DocumentTextIcon className="h-5 w-5" />
               <span>Historial de programaciones</span>
             </a>
-  
+
+
             <a
               href="#"
-              className="flex items-center space-x-3 px-3 py-2 rounded-md text-sky-900 hover:bg-gray-100"
+              className="flex items-center space-x-3 px-3 py-2 rounded-md text-sky-900 hover:bg-gray-100 font-medium"
             >
-              <BuildingOffice2Icon className="h-5 w-5" />
-              <span>Gestión de aulas</span>
+              <Squares2X2Icon className="h-5 w-5" />
+              <span>Acerca de</span>
             </a>
   
-            <a
-              href="#"
-              className="flex items-center justify-between px-3 py-2 rounded-md text-sky-900 hover:bg-gray-100"
-            >
-              <div className="flex items-center space-x-3">
-                <UsersIcon className="h-5 w-5" />
-                <span>Gestión de grupos</span>
-              </div>
-            </a>
+
   
             <a
               href="#"
@@ -69,8 +73,18 @@ import {
               </div>
               <ChevronDownIcon className="h-4 w-4 text-gray-500" />
             </a>
+
+            <a
+              href="#"
+              className="flex items-center space-x-3 px-3 py-2 rounded-md text-sky-900 hover:bg-gray-100"
+            >
+              <BuildingOffice2Icon className="h-5 w-5" />
+              <span>Gestión de aulas</span>
+            </a>
           </nav>
         </div>
+
+
   
         {/* User section */}
         <div className="p-6">
