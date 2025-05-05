a80275bd7d8c603fdfd47e4525c3c577e4c7b4b7
julianisazam <julian.isazam@udea.edu.co>
2025-05-05 16:46:00 -0500

    refactor: clean up imports and improve code formatting in PensumPage component

--- src/app/pensum/page.tsx
+++ src/app/pensum/page.tsx
@@ -1,32 +1,35 @@
 'use client';
-import CustomDropdown from "@/components/util/CustomDropdown"
-import {CustomDataGrid} from "@/components/util/CustomDataGrid"
-import { useState } from "react"
-import {Pensum} from "../../interface/Pensum"
-import {usePensums} from "../../hooks/usePensums"
+import CustomDropdown from "@/components/util/CustomDropdown";
+import { CustomDataGrid } from "@/components/util/CustomDataGrid";
+import { useState } from "react";
+import { Pensum } from "../../interface/Pensum";
+import { usePensums } from "../../hooks/usePensums";
 import { useSubject } from "@/hooks/useSubject";
+import { useAcademicProgram } from "@/hooks/useAcademicProgram"; // <-- importar el hook
 
+const PensumPage = () => {
+  const { pensums, isLoading } = usePensums();
+  const [selectedPensum, setSelectedPensum] = useState<Pensum | null>(null);
+  const { subjects, isLoading: loadingSubject } = useSubject(selectedPensum?.id || 0);
+  const { academicPrograms, isLoading: loadingPrograms } = useAcademicProgram(); 
 
- const  PensumPage = () => {
-        
-    const {pensums, isLoading} = usePensums()
-    const [selectedPensum, setSelectedPensum] = useState<Pensum | null>(null)
-    const {subjects, isLoading: loadingSubject} = useSubject(selectedPensum?.id || 0)
+  if (isLoading || loadingPrograms) return <div>Loading...</div>;
 
-    if (isLoading) return <div>Loading...</div> 
-    return (
-        <>
-            <div>Pensum</div>
-            <CustomDropdown
-                pensums={pensums}
-                selectedPensum={selectedPensum}
-                onSelect={setSelectedPensum}
-            />
-             
-             <div className="container mx-auto mt-4 pl-1">
-                <CustomDataGrid data={subjects}/>
-            </div>
-        </>
-    )
-} 
+  return (
+    <>
+      <div>Pensum</div>
+      <CustomDropdown
+        pensums={pensums}
+        selectedPensum={selectedPensum}
+        onSelect={setSelectedPensum}
+        academicPrograms={academicPrograms} 
+      />
+
+      <div className="container mx-auto mt-4 pl-1">
+        <CustomDataGrid data={subjects} />
+      </div>
+    </>
+  );
+};
+
 export default PensumPage;
