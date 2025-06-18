eb8659bc1262c364e5f9bdf08593fe4f01d11404
Jaime Muñoz <jaime.munozq@udea.edu.co>
2025-06-17 21:43:53 -0500

    refactor: clean up imports and improve code formatting in various components

--- src/app/academic-schedule/components/GroupNotification.tsx
+++ src/app/academic-schedule/components/GroupNotification.tsx
@@ -1,38 +1,38 @@
 import { useGroupNotifications } from "@/hooks/useGroupNotification"
 import { GroupResponse } from "@/interface/Group"
-import {BellAlertIcon} from "@heroicons/react/24/outline";
+import { BellAlertIcon } from "@heroicons/react/24/outline";
 import { Tooltip } from "@heroui/react";
 
 interface GroupNotificationProps {
-    group: GroupResponse
+  group: GroupResponse
 }
 
 export const GroupNotification = ({
-    group
-    }: GroupNotificationProps) => {
+  group
+}: GroupNotificationProps) => {
 
-      const {data} = useGroupNotifications(group.id)
+  const { data } = useGroupNotifications(group.id)
 
-      const handleColor = (messageName: string) => {
-        switch (messageName) {
-          case "SCHEDULE MODIFIED":
-            return "text-yellow-500";
-          case "CLASSROOM MODIFIED":
-            return "text-blue-500";
-          case "CLASSROOM SET":
-            return "text-green-500";
-          case "COLLISION":
-            return "text-red-500";
-          case "CAPACITY EXCEEDED":
-            return "text-orange-500";
-          case "MIRROR GROUP SCHEDULE MODIFIED":
-            return "text-purple-500";
-          case "MIRROR GROUP CLASSROOM MODIFIED":
-            return "text-pink-500";
-          default:
-            return "text-gray-500";
-        }
-      }
+  const handleColor = (messageName: string) => {
+    switch (messageName) {
+      case "SCHEDULE MODIFIED":
+        return "text-amber-600";
+      case "CLASSROOM MODIFIED":
+        return "text-blue-600";
+      case "CLASSROOM SET":
+        return "text-green-600";
+      case "COLLISION":
+        return "text-red-600";
+      case "CAPACITY EXCEEDED":
+        return "text-orange-600";
+      case "MIRROR GROUP SCHEDULE MODIFIED":
+        return "text-purple-600";
+      case "MIRROR GROUP CLASSROOM MODIFIED":
+        return "text-pink-600";
+      default:
+        return "text-gray-600";
+    }
+  }
 
   return (
     <div>
@@ -41,7 +41,12 @@ export const GroupNotification = ({
           {data.map((notification) => (
             <Tooltip
               key={notification.id}
-              content={notification.detail}
+              content={
+                <div className="text-sm text-gray-700">
+                  <strong>{notification.messageType.name}</strong>
+                  <div>{notification.detail}</div>
+                </div>
+              }
               className="w-64"
             >
               <div className="flex items-center gap-2">
